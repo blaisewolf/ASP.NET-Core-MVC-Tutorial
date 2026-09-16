504d69ea3f6c1b5c712433722b166069fc40af95
Blaise Wolf <blaiseywolf@gmail.com>
2026-09-16 03:43:01 +0200

    Add actions, parameters to HelloWorld/Welcome

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
@@ -11,9 +12,9 @@ namespace MvcMovie.Controllers
         }
 
         // GET: /HelloWorld/Welcome/
-        public string Welcome()
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
